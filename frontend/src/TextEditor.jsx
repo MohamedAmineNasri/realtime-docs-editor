1ab92048df8839317ffdbf243e25a4942328e3f3
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2025-06-23 10:48:36 +0100

    style: apply custom CSS for Quill editor and print layout
    - Styled Quill editor container for centered A4-like layout- Customized toolbar and editor background with box shadow- Added sticky toolbar with shadow for better UX- Optimized print styles using @page and @media print rules- Ensured consistent spacing, alignment, and readability

--- frontend/src/TextEditor.jsx
+++ frontend/src/TextEditor.jsx
@@ -1,6 +1,20 @@
 import { useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
+
 export default function TextEditor() {
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
@@ -10,8 +24,10 @@ export default function TextEditor() {
 
     wrapper.append(editor);
     new Quill(editor, {
-      theme: "snow",
+      theme: "snow", modules:{
+        toolbar: TOOLBAR_OPTIONS
+      }
     });
   }, []);
-  return <div id="container" ref={wrapperRef}></div>;
+  return <div className="container" ref={wrapperRef}></div>;
 }
